8c1df921164445f55d77412f6cf76880003223d4
macinfa <893810797@qq.com>
2017-02-28 13:18:41 +0800

    添加注释

--- app/src/main/java/com/example/hasee/testgit/MainActivity.java
+++ app/src/main/java/com/example/hasee/testgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //添加注释
+        //添加注释5656
     }
 }
